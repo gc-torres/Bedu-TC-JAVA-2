df035fff827268d372763f5d0b9aded68f165c63
gc-torres <gretelclara28@gmail.com>
2025-06-14 15:54:48 -0600

    Añadidura del reto_1 y reto_2

---
+++ Sesion_3/Reto_2/Sucursal.java
@@ -0,0 +1,20 @@
+package Sesion_3.Reto_2;
+
+import java.util.List;
+public class Sucursal {
+    private String nombre;
+    private List<Encuesta> encuestas;
+
+    public Sucursal(String nombre, List<Encuesta> encuestas){
+        this.nombre = nombre;
+        this.encuestas = encuestas;
+    }
+
+    public String getNombre(){
+        return nombre; 
+    }
+
+    public List<Encuesta> getEncuestas(){
+        return encuestas; 
+    }
+}
