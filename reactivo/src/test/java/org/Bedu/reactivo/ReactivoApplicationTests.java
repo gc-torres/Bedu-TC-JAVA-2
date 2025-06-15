61809f9875ff1a191250015965e478388818096d
gc-torres <gretelclara28@gmail.com>
2025-06-15 17:18:47 -0600

    Añadidura de los retos de la sesion 5 así como de la carpeta de gladlew para correrlos

---
+++ reactivo/src/test/java/org/Bedu/reactivo/ReactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Bedu.reactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
